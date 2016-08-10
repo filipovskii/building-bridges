261f20acf6c23c383d6251201bc6cb3cd4c2a887
Andrey Salomatin <filipovskii.off@gmail.com>
2016-08-10 14:28:56 +0200

    All the examples.

--- WarsawNative/WarsawNative/WarsawNative/JSVirtualMachineController.swift
+++ WarsawNative/WarsawNative/WarsawNative/JSVirtualMachineController.swift
@@ -6,6 +6,14 @@
 //  Copyright © 2016 Andrey Salomatin. All rights reserved.
 //
 
+
+// Example to try
+// for (var i = 0; i < 1000) {
+//    Math.random() / Math.random();
+// }
+//
+
+
 import UIKit
 import JavaScriptCore
 
@@ -18,6 +26,18 @@ class JSVirtualMachineController: UIViewController {
   
   @IBAction func onRunClick(sender: AnyObject) {
     let input = jsInput.text;
+    
+//    let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT;
+//    dispatch_async(dispatch_get_global_queue(priority, 0)) {
+//        let output = self.jsContext?.evaluateScript(input);
+//
+//      dispatch_async(dispatch_get_main_queue()) {
+//        self.outputInput.text = String(format: "%@", output!);
+//      }
+//    }
+//    return;
+    
+    
     let output = self.jsContext?.evaluateScript(input);
     outputInput.text = String(format: "%@", output!);
   }
